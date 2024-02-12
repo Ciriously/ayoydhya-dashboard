d6011136e46bb6d36ecd5b9023f36e5a6078baba
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2024-02-12 19:53:04 +0530

    Initial commit

---
+++ src/pages/Tables.tsx
@@ -0,0 +1,19 @@
+import Breadcrumb from '../components/Breadcrumbs/Breadcrumb';
+
+import TableThree from '../components/Tables/TableThree';
+
+import DefaultLayout from '../layout/DefaultLayout';
+
+const Tables = () => {
+  return (
+    <DefaultLayout>
+      <Breadcrumb pageName="Customer Orders" />
+
+      <div className="flex flex-col gap-10">
+        <TableThree />
+      </div>
+    </DefaultLayout>
+  );
+};
+
+export default Tables;
